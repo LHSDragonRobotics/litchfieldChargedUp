4c8861bb2f28ad7faa063d47b661eedeb7146d51
Dragon <DragonRobotics@outlook.com>
2024-01-11 15:10:46 -0600

    Initial Commit

---
+++ src/main/java/frc/robot/commands/RobotDrive.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class RobotDrive extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DriveSubsystem m_subsystem;
+  public double divrate = 1;
+  public double rate = 0.1;
+
+  private XboxController xbox = RobotContainer.m_driverController;
+
+  private int printCount = 0;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public RobotDrive() {
+    m_subsystem = RobotContainer.m_robotDrive ;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (xbox.getAButton()) {
+      divrate = 0.1;
+    } else {
+      divrate = 0.3;
+    }
+
+    double JS_BIAS_X = 0; //  double JS_BIAS_X = .3 
+
+    // compute a deadband-safe speed for each direction of control.
+    double xrate = xbox.getLeftX() + JS_BIAS_X ;  //double xrate = xbox.getLeftX() + JS_BIAS_X ;
+    xrate *= Math.abs(xrate * divrate); // competition rate is .8 - The lower the decimal the slower it drives
+    double yrate = xbox.getLeftY();
+    yrate *= Math.abs(yrate * divrate); // competition rate is .8 - The lower the decimal the slower it drives
+
+    double zRate = xbox.getRawAxis(4)*.5;
+
+    if ( printCount++ % 55 == 0 )
+        System.out.println("  DRIVE " + xrate + ", " + yrate + " (" + zRate + ")");
+
+
+     m_subsystem.drive(-yrate, -xrate, -zRate, false, true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {} 
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
